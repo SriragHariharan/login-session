b6ada6ebd00de84d7b7671b7ce31629fbc2a3efc
Srirag Hariharan <sriraghariharan108@gmail.com>
2024-04-11 10:40:27 +0530

    Added express-handlebars as the template engine and setup main hbs file and homepage hbs file to verfiy its working.

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const hbs = require('express-handlebars');
+const path = require('node:path')
+
 const app = express();
 
 require('dotenv').config();
 
+//view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs.engine({extname:'hbs', defaultLayout:'main', layoutsDir:__dirname+'/views/layout/', partialsDir:__dirname+'/views/partials/'}));
+
+
+
 app.get('/', (req, res) => {
-    res.json({message: "Hello world", author:"Srirag"})
+    res.render('home', {desc:"lorem ipsum doler sit amet is a simple dummy text.!"})
 })
 
 app.listen(process.env.PORT, () => console.log(`Server started at http://localhost:${process.env.PORT}`))
